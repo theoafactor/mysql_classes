92bcc7903fec47500781286d17f979c191bd6655
Olu Adeyemo <theoafactor@gmail.com>
2025-08-19 11:45:40 +0100

    Working with users

---
+++ read_users.sh
@@ -0,0 +1,25 @@
+#!/bin/bash 
+
+datafile="data.csv"
+
+
+while read line;
+    do 
+        # echo $line;
+
+        if [[ $line == "Firstname,Lastname,Email,Username,Password" ]]
+        then 
+            echo "Ignoring the headers ..." 
+        else 
+            firstname=$(echo $line | cut -d "," -f1 )
+            lastname=$(echo $line | cut -d "," -f2)
+            email=$( echo $line | cut -d "," -f3)
+            username=$( echo $line | cut -d "," -f4)
+            password=$( echo $line | cut -d "," -f5)
+
+
+            sudo useradd $username 
+
+
+        fi  
+done < $datafile
